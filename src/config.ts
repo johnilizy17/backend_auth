cd117ec4d4ef98c3deba2df2c0043f873068ddee
unknown <johnabrahamtosin@gmail.com>
2022-09-30 14:13:15 +0100

    the first commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ||
+  "361416488628-9dmes9ums84h578pr18pjji9cg80897k.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET || "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";
+export const SERVER_ROOT_URI = "http://localhost:4000";
+export const UI_ROOT_URI = "http://localhost:3001";
+export const JWT_SECRET = "shhhhh";
+export const COOKIE_NAME = "auth_token";
